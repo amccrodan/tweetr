7b40a88742b8914412218ccea3598e1a42c3d5bc
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-24 02:18:01 +0000

    Created script with JQuery to count number of characters in text box and update counter accordingly.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".new-tweet").on("keyup", "textarea", function() {
+    const remainingChars = 140 - $(this).val().length;
+    const counter = $(this).parent().find('.counter');
+
+    if (remainingChars < 0) {
+      counter.addClass("invalid");
+    } else {
+      counter.removeClass("invalid");
+    }
+
+    counter.text(remainingChars);
+  });
+});
