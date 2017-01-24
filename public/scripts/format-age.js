5ea7c7651e6096ebd7204d522c356fd847532a71
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-24 22:07:31 +0000

    Created separate script for tweet age formatting.

---
+++ public/scripts/format-age.js
@@ -0,0 +1,34 @@
+function formatAge(creationTime) {
+  let dateString = "";
+
+  const msInMinute = 60000;
+  const msInHour = msInMinute * 60;
+  const msInDay = msInHour * 24;
+  const msInYear = msInDay * 365.25;
+
+  const dateNow = Date.now();
+  const elapsedTime = dateNow - creationTime;
+
+  switch(true) {
+    case (elapsedTime < msInMinute):
+      dateString = "Less than one minute ago"
+    break;
+    case (elapsedTime < msInHour):
+      dateString = Math.floor(elapsedTime / msInMinute) + " minutes"
+    break;
+    case (elapsedTime < msInDay):
+      dateString = Math.floor(elapsedTime / msInHour) + " hours"
+    break;
+    case (elapsedTime < msInYear):
+      dateString = Math.floor(elapsedTime / msInDay) + " days"
+    break;
+    default:
+      dateString = Math.floor(elapsedTime / msInYear) + " years"
+  }
+
+  if (dateString.split(" ")[0] === "1") {
+    dateString = dateString.replace("s", "");
+  }
+
+  return dateString + " ago";
+}
