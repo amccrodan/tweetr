2f317f2bf47291ba3934848323a90d8ac7ce1127
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-25 02:19:58 +0000

    Added comments, made tweets clear and re-render upon tweet submission (implemented with $.post()). Removed template data. Implemented Compose button toggle functionality.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(function() {
 
+  // Create tweet DOM representation
   function createHeader(tweetObject) {
     const $header = $("<header>").addClass("faded");
 
@@ -52,60 +53,58 @@ $(document).ready(function() {
     return $tweet;
   }
 
+  // Render tweets on the page
+  function clearTweets() {
+    $('#tweet-list').empty();
+  }
+
   function renderTweets(tweets) {
+
+    clearTweets();
+
+    tweets = tweets.sort(function(a, b) {
+      return b.created_at - a.created_at;
+    });
+
     for (let tweetObject of tweets) {
       $('#tweet-list').append(createTweetElement(tweetObject));
     }
   }
 
-  // Fake data taken from tweets.json
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1485294208659
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1485294208659 - 3600000
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1485294208659 - 200000000
+  function loadTweets() {
+    $.getJSON("/tweets", function(data) {
+      renderTweets(data);
+    })
+  }
+  loadTweets();
+
+  // Submit new tweet
+  $(".new-tweet form").submit(function (event) {
+    event.preventDefault();
+
+    const numChars = $(this).find("textarea").val().length;
+
+    if (numChars === 0) {
+      alert("You may not submit an empty tweet.");
+      return;
     }
-  ];
 
-  renderTweets(data);
+    if (numChars > 140) {
+      alert("Too many characters.");
+      return;
+    }
 
+    $.post("/tweets", $(this).serialize(), function(){
+      loadTweets();
+    });
+  });
+
+  // Compose button
+  $(".compose").on("click", function() {
+    $(".new-tweet").slideToggle(function() {
+      if ($(".new-tweet").is(":visible")) {
+        $(".new-tweet").find("textarea").focus();
+      }
+    });
+  });
 });
