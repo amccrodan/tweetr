c418c5b79c799bdd37de8900fd6ed67ff57ed3d4
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-26 20:42:37 +0000

    Added event check before listeners are added to individual tweets. Added class toggling to all icons and ajax for like button.

--- public/scripts/tweet.js
+++ public/scripts/tweet.js
@@ -1,15 +1,55 @@
 $(function() {
   const $tweetList = $("#tweet-list");
 
-  $tweetList.on("mouseenter", "article", function () {
-    const header = $(this).find($("header"));
-    const icons = $(this).find($(".icons"));
-    icons.addClass("tweet-hover");
-    header.removeClass("faded");
-  }).on("mouseleave", "article", function () {
-    const header = $(this).find($("header"));
-    const icons = $(this).find($(".icons"));
-    icons.removeClass("tweet-hover");
-    header.addClass("faded");
+  $tweetList.on("tweetsLoaded", function () {
+
+    $tweetList.on("mouseenter", "article", function () {
+      const header = $(this).find($("header"));
+      const icons = $(this).find($(".icons"));
+      icons.addClass("tweet-hover");
+      header.removeClass("faded");
+    }).on("mouseleave", "article", function () {
+      const header = $(this).find($("header"));
+      const icons = $(this).find($(".icons"));
+      icons.removeClass("tweet-hover");
+      header.addClass("faded");
+    });
+
+    const $flag = $tweetList.find(".fa-flag");
+    const $retweet = $tweetList.find(".fa-retweet");
+    const $heart = $tweetList.find(".fa-heart");
+
+    $flag.on("click", function () {
+      $(this).toggleClass("flagged");
+    });
+
+    $retweet.on("click", function () {
+      $(this).toggleClass("retweeted");
+    });
+
+    $heart.on("click", function () {
+      let liked = false;
+      let increment = 0;
+      const $likes = $(this).closest(".icons").find(".likes");
+
+      if ($(this).hasClass("liked")) {
+        $(this).removeClass("liked");
+        liked = false;
+        increment = -1;
+      } else {
+        $(this).addClass("liked");
+        liked = true;
+        increment = 1;
+      }
+
+      $.ajax({
+        url: `/tweets/${$(this).data("tweet-id")}`,
+        method: "POST",
+        data: {liked: liked}
+      }).then(function () {
+        const newLikes = Number($likes.text()) + increment;
+        $likes.text(newLikes);
+      });
+    });
   });
 });
