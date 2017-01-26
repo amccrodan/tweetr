abda9a55859c3971fb9de45254fe3cdd1626404f
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-26 02:50:15 +0000

    Added some spacing and $ to variable name.

--- public/scripts/tweet.js
+++ public/scripts/tweet.js
@@ -1,12 +1,12 @@
 $(function() {
-  const tweetList = $("#tweet-list");
+  const $tweetList = $("#tweet-list");
 
-  $(tweetList).on("mouseenter", "article", function() {
+  $tweetList.on("mouseenter", "article", function () {
     const header = $(this).find($("header"));
     const icons = $(this).find($(".icons"));
     icons.addClass("tweet-hover");
     header.removeClass("faded");
-  }).on("mouseleave", "article", function() {
+  }).on("mouseleave", "article", function () {
     const header = $(this).find($("header"));
     const icons = $(this).find($(".icons"));
     icons.removeClass("tweet-hover");
