caa16440a5e51d134fbdf5bffd9fd50b3f0f97ae
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-26 02:26:15 +0000

    Added # likes to db objects, and post route for individual tweets.

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper    = require("../lib/util/user-helper");
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
@@ -29,7 +29,8 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: Date.now(),
+      likes: 0
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
@@ -41,6 +42,13 @@ module.exports = function(DataHelpers) {
     });
   });
 
+  tweetsRoutes.post("/:id", function(req, res) {
+    if (!req.body.text) {
+      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      return;
+    }
+  });
+
   return tweetsRoutes;
 
-}
+};
